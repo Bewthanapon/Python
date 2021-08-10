702e42968d3f6c05bb6cc094b4fdbb08ac334929
Thanapon Vichitsrikamol <72264147+Bewthanapon@users.noreply.github.com>
2021-08-10 16:16:29 +0700

    Add files via upload

---
+++ autobrowser.py
@@ -0,0 +1,25 @@
+# pip install PyAutoGUI
+
+import webbrowser
+import pyautogui as pg
+import time
+
+def Search(keyword):
+	# 1- open webbrower and goto google
+	url = 'https://www.google.com'
+	webbrowser.open(url)
+	time.sleep(1)
+
+	# 2- type "keyword"
+	pg.write(keyword, interval = 0.25)
+	time.sleep(1)
+
+	# 3- press enter for searching
+	pg.press('enter')
+	time.sleep(1)
+
+	# 4- capture (screenshot) and save to file
+	pg.screenshot(keyword + '.png')
+
+Search('bill gate')
+Search('steve job')	
